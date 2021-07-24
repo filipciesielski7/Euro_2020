66856862c4cd967e8d4761bc44de3ac46e68b805
filipciesielski7 <filipcies@gmail.com>
2021-07-25 00:43:09 +0200

    Back to hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Team from "./pages/Team";
 import Error from "./pages/Error";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename='/'>
       <Switch>
         <Route exact path="/">
           <Home />
