4454ffc27e7fcd44957a7cb3a0887068abf1d7bd
filipciesielski7 <filipcies@gmail.com>
2021-07-25 00:10:45 +0200

    Modified routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Team from "./pages/Team";
 import Error from "./pages/Error";
