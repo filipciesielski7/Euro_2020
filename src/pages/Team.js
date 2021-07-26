5d07ca30ddbf9466f460ce07dab340cfe6b5bf23
filipciesielski7 <filipcies@gmail.com>
2021-07-26 23:50:40 +0200

    Added subpage navbar

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { FaBackward } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import { useGlobalContext } from "../context";
 
 const Team = () => {
   const { code } = useParams();
-  const { teams } = useGlobalContext();
+  const { teams, flags } = useGlobalContext();
+
   let name, group;
   teams.forEach((team) => {
     if (team.code === code) {
@@ -13,17 +15,40 @@ const Team = () => {
     }
   });
 
+  let symbol;
+  flags.forEach((element) => {
+    if (element.name === name) {
+      symbol = element.symbol;
+    }
+  });
+  let flagUrl;
+  if (symbol !== undefined) {
+    flagUrl = `https://flagcdn.com/h240/${symbol.replace(/_/g, "-")}.png`;
+  }
+
   return (
     <div className="single-team-container">
       <div className="single-team-section">
         <Link to="/" className="single-team-btn">
-          Back Home
+          <div className="back-icon">
+            <FaBackward />
+          </div>
+          <span className="back-info">Back Home</span>
         </Link>
-        <div className="single-team-title">
-          <h2 className="single-team-name">{name}</h2>
-          <h2 className="single-team-code">{code}</h2>
-          <h2 className="single-team-group">{group}</h2>
+        <div className="subnav">
+          <div className="single-team-title">
+            <h2 className="single-team-name">{name}</h2>
+            <h2 className="single-team-code">{code}</h2>
+            <h2 className="single-team-group">{group}</h2>
+          </div>
+          <div
+            className="single-team-team"
+            style={{
+              backgroundImage: `url(${flagUrl})`,
+            }}
+          ></div>
         </div>
+        <div className="sub-main"></div>
       </div>
     </div>
   );
